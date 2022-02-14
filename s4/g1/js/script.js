e8f70b73c277386f616decf2cb6eaa7969740d1c
M1rkDev <mirko.saba@gmail.com>
2022-02-14 17:49:00 +0100

    esercizios4g1

---
+++ s4/g1/js/script.js
@@ -0,0 +1,6 @@
+function cambiacolore () {
+    document.getElementById('colore').style.color = '#fff';
+    document.getElementById('colore1').style.color = '#fff';
+    document.getElementById('tasto').style.backgroundColor = '#fff';
+}
+
